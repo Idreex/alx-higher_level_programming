fbd1146ce5839e5e9145bd5e010e51267f8e0460
Idreex <Idreesademola07@gmail.com>
2022-08-22 18:55:38 -0700

    file

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-<<<<<<< HEAD
 print(str *3)
 print(str[:9])
-=======
-print(str + str + str)
-print("str[:9]")
->>>>>>> 9a071fe08ee6df35f1854b2b358a4027964fbc26
