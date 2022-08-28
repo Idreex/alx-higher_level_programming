8ec507988dbe5c14a4d1b2ec03d839e5a1eaecda
Idreex <Idreesademola07@gmail.com>
2022-08-28 16:05:29 -0700

    hot fix

--- 0x02-python-import_modules/0-add_commented.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__": #e y no cuando es importado (ya sea por otro script o lo que sea). La letra del ej nos pide
-    # que pongamos esa condicion.
-    from add_0 import add #os la funcion "add" del modulo "add_0" que creamos en otro archivo.
-    a = 1
-    b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
