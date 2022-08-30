0fed240fcee6ce32bb640a212573f8740a2b2f91
Idreex <Idreesademola07@gmail.com>
2022-08-29 18:01:18 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
 
-def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{:d}".format(item))
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
